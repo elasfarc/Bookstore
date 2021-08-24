30ac3c51ec4ddd650967e792f619d8ebd8cb35f7
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-24 21:42:42 +0200

    Add access to store in the App

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
 import Library from './components/Library';
 import Categories from './components/Categories';
+import store from './store/configureStore';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Library} />
-        <Route path="/categories" exact component={Categories} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Library} />
+          <Route path="/categories" exact component={Categories} />
+        </Switch>
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
