fb7861e9ec6aa4e2e0892808eaff94972eb29054
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-08-27 11:07:13 +0200

    Book component styles -- final touches

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,37 +7,42 @@ const Book = ({ book }) => {
   const dispatch = useDispatch();
 
   return (
-    <div class="book">
-      <div class="flex space-between cross-center">
-        <div class="flex direction-col grow">
-          <div class="book--info">
-            <p class="book--category">action</p>
-            <h4 class="book--title">the hunger games</h4>
-            <p class="book--author">suzanne collins</p>
+    <div className="book">
+      <div className="flex space-between cross-center">
+        <div className="flex direction-col grow">
+          <div className="book--info">
+            <p className="book--category">{book.category}</p>
+            <h4 className="book--title">{book.title}</h4>
+            <p className="book--author">suzanne collins</p>
           </div>
-          <div class="book--control flex gap-2">
-            <a href="" class="btn btn__book-ctrl vertical-pipe">
+          <div className="book--control flex gap-2">
+            <a href="#" className="btn btn__book-ctrl vertical-pipe">
               comment
             </a>
-            <a href="" class="btn btn__book-ctrl vertical-pipe">
+            <button
+              type="button"
+              className="btn btn__book-ctrl vertical-pipe"
+              onClick={() => dispatch(actions.removeBook(book.id))}
+            >
               remove
-            </a>
-            <a href="" class="btn btn__book-ctrl">
+            </button>
+            <a href="#" className="btn btn__book-ctrl">
               edit
             </a>
           </div>
         </div>
-        <div class="book--status flex cross-center gap-3">
-          <div class="book--progress flex cross-center gap-1 vertical-pipe">
-            <div class="book--progress--chart"></div>
-            <div class="book--progress--plain">
-              64% <span>completed</span>
+        <div className="book--status flex cross-center gap-3">
+          <div className="book--progress flex cross-center gap-1 vertical-pipe">
+            <div className="book--progress--chart" />
+            <div className="book--progress--plain">
+              64%
+              <span>completed</span>
             </div>
           </div>
-          <div class="book--current-state">
-            <p class="book--current-chp">current chapter</p>
-            <p class="book--current-chp-name">chapter 17</p>
-            <a href="" class="btn btn__book-state">
+          <div className="book--current-state">
+            <p className="book--current-chp">current chapter</p>
+            <p className="book--current-chp-name">chapter 17</p>
+            <a href="" className="btn btn__book-state">
               update progress
             </a>
           </div>
